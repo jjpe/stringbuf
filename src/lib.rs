6ad6b116c9b4c93924838b138a424d4d2060cdb9
Joey Ezechiëls <joey.ezechiels@gmail.com>
2017-02-04 15:39:48 +0100

    Add tests for instantiation and appending

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ impl StringBuf {
     }
 }
 
+
 impl From<String> for StringBuf {
     fn from(string: String) -> StringBuf {  StringBuf(string)  }
 }
@@ -36,17 +37,23 @@ mod tests {
     use *;
 
     #[test]
+    fn instantiation() {
+        let sb = StringBuf::new();
+        assert_eq!("", String::from(sb))
+    }
 
     #[test]
-    fn it_works() {
-        let mut buf = StringBuf::new();
-        buf = buf + "foo ";
-        buf = buf + "bar ";
-        buf = buf.append("baz ").append("qux!");
+    fn append_by_method() {
+        let sb = StringBuf::new().append("foo").append("bar!");
+        assert_eq!("foobar!", String::from(sb));
+    }
 
-        let s = "BLAH!!!".to_string();
-        buf = buf + &*s;
-        println!("buf: {:?}", String::from(buf));
-        assert!(false);
+
+    #[test]
+    fn append_by_operator() {
+        let sb = StringBuf::new();
+        let s = String::from("baz!");
+        let sb = sb + "foo" + "bar!" + &*s;
+        assert_eq!("foobar!baz!", String::from(sb));
     }
 }
